a3a40105dc44d03865831ce19f373a0e51e2a6a9
Jeferson Barros <im.jbalves@gmail.com>
2016-11-04 19:29:44 -0400

    Initial layout details

--- app/src/main/java/barros/jeferson/avaliadentista/MainActivity.java
+++ app/src/main/java/barros/jeferson/avaliadentista/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void setBottomNavigation() {
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
