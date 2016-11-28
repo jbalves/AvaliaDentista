d47269f69a0e222da069be4e9747af0a6c8328aa
Jeferson Barros <im.jbalves@gmail.com>
2016-11-28 19:29:37 -0400

    Remove code not used

--- app/src/main/java/barros/jeferson/avaliadentista/fragments/ConsultasFragment.java
+++ app/src/main/java/barros/jeferson/avaliadentista/fragments/ConsultasFragment.java
@@ -17,13 +17,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
-import com.oceanbrasil.libocean.Ocean;
-import com.oceanbrasil.libocean.control.http.Request;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -31,13 +24,12 @@ import barros.jeferson.avaliadentista.R;
 import barros.jeferson.avaliadentista.adapter.AgendamentoAdapter;
 import barros.jeferson.avaliadentista.adapter.MyAdapter;
 import barros.jeferson.avaliadentista.model.Agendamento;
-import barros.jeferson.avaliadentista.model.UnidadeSaude;
 
-/**
+ /**
  * Created by Jeferson Barros <im.jbalves@gmail.com> on 11/4/16.
  */
 
-public class ConsultasFragment extends Fragment implements Request.RequestListener {
+public class ConsultasFragment extends Fragment {
 
     private View mView;
     private DatabaseReference mDatabase;
@@ -63,48 +55,12 @@ public class ConsultasFragment extends Fragment implements Request.RequestListen
         getAgendamentos();
     }
 
-    @Override
-    public void onRequestOk(String resposta, JSONObject jsonObject, int code) {
-         if (code == Request.NENHUM_ERROR) {
-             stringToJson(resposta);
-             criarAdapter(mView, mLista);
-         }
-    }
-
-    public void stringToJson (String resposta) {
-        ArrayList<UnidadeSaude> lista = new ArrayList<>();
-
-        if (resposta != null) {
-            try {
-
-                JSONArray unidades = new JSONArray(resposta);
-
-                for (int i = 0; i < unidades.length(); i++) {
-                    JSONObject unidade = unidades.getJSONObject(i);
-                    String nomeFantasia = unidade.getString("nomeFantasia");
-
-                    UnidadeSaude unidadeSaude = new UnidadeSaude();
-                    unidadeSaude.setNome(nomeFantasia);
-                    unidadeSaude.setDataAtendimento("07/11/2016");
-                    unidadeSaude.setDiasEspera(10);
-                    unidadeSaude.setRating((float)3.0);
-
-                    //mLista.add(unidadeSaude);
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     private void criarAdapter(View view, ArrayList<Agendamento> lista) {
         AgendamentoAdapter adapter = new AgendamentoAdapter(view.getContext(), lista);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.lista_consulta_recyclerview);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
-
 
     public void getAgendamentos(){
 
